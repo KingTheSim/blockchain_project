863f8d9f6c829b95eacd56bcc0c68858d24d72e3
simeon <kingthesim@abv.bg>
2023-12-31 00:43:36 +0200

    blockchain_database_initial

--- body/rs_blockchain/src/lib.rs
+++ body/rs_blockchain/src/lib.rs
@@ -1,20 +1,24 @@
 use core::time;
-use std::ffi::{CString, CStr};
-use std::os::raw::c_char;
 use chrono::{Utc, DateTime};
 use blake3::Hasher;
+use rusqlite::{params, Connection};
+use serde::{Serialize, Deserialize};
+use serde_json::json;
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Block {
     index: usize,
-    timestamp: *const c_char,
-    proof: *const c_char,
-    hash: *const c_char,
-    previous_hash: *const c_char,
-    encoded_model: *const c_char,
+    timestamp: String,
+    proof: usize,
+    hash: String,
+    previous_hash: String,
+    encoded_model: String,
 }
 
 impl Block {
-    fn new(index: usize, timestamp: *mut i8, proof: *mut i8, hash: *mut i8, previous_hash: *const i8, encoded_model: *mut i8) -> Self {
+    fn new(index: usize, timestamp: String, proof: usize, previous_hash: String, encoded_model: String) -> Self {
+        let hash = Block::calculate_hash(index, &timestamp, proof, &previous_hash, &encoded_model);
+
         Block {
             index,
             timestamp,
@@ -25,14 +29,14 @@ impl Block {
         }
     }
 
-    fn calculate_hash(index: usize, timestamp: *const c_char, proof: *const c_char, previous_hash: *const c_char, encoded_model: *const c_char) -> String {
+    fn calculate_hash(index: usize, timestamp: &String, proof: usize, previous_hash: &String, encoded_model: &String) -> String {
         let content = format!(
             "{}{}{}{}{}",
             index,
-            unsafe { CStr::from_ptr(timestamp).to_string_lossy() },
-            unsafe { CStr::from_ptr(proof).to_string_lossy() },
-            unsafe { CStr::from_ptr(previous_hash).to_string_lossy() },
-            unsafe { CStr::from_ptr(encoded_model).to_string_lossy() }
+            timestamp,
+            proof,
+            previous_hash,
+            encoded_model
         );
 
         let mut hasher = Hasher::new();
@@ -40,16 +44,25 @@ impl Block {
         let hash = hasher.finalize();
         hash.to_hex().to_string()
     }
+
+    fn to_json(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+
+    fn from_json(json: &str) -> Block {
+        serde_json::from_str(json).unwrap()
+    }
 }
 
 impl Default for Block {
     fn default() -> Block {
         let index = 0;
-        let timestamp = CString::new(Utc::now().to_rfc3339()).unwrap().into_raw();
-        let proof = CString::default().into_raw();
-        let hash = CString::default().into_raw();
-        let previous_hash = CString::default().into_raw();
-        let encoded_model = CString::default().into_raw();
+        let timestamp = Utc::now().to_rfc3339();
+        let proof = usize::default();
+        let previous_hash = String::default();
+        let encoded_model = String::default();
+
+        let hash = Block::calculate_hash(index, &timestamp, proof, &previous_hash, &encoded_model);
 
         Block {
             index,
@@ -63,25 +76,88 @@ impl Default for Block {
 }
 
 pub struct BlockChain {
-    blocks: Vec<Block>,
+    connection: Connection,
 }
 
 impl BlockChain {
-    pub fn new() -> Self {
-        BlockChain { blocks: vec![Block::default()] }
+    pub fn new() -> Result<Self, rusqlite::Error> {
+        let connection = Connection::open("blockchain.db")?;
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS blocks (
+                index INTEGER PRIMARY KEY,
+                timestamp TIMESTAMP UNIQUE NOT NULL,
+                proof INTEGER NOT NULL,
+                hash TEXT UNIQUE NOT NULL,
+                previous_hash TEXT UNIQUE NOT NULL,
+                encoded_model TEXT NOT NULL
+            )",
+            []
+        )?;
+
+        let is_empty: bool = connection.query_row(
+            "SELECT COUNT(*) FROM blocks",
+            [],
+            |row| row.get(0)
+        )?;
+
+        if is_empty {
+            let block = Block::default();
+            connection.execute(
+                "INSERT INTO blocks (index, timestamp, proof, hash, previous_hash, encoded_model)
+                VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+                params![
+                    block.index,
+                    &block.timestamp,
+                    block.proof,
+                    &block.hash,
+                    &block.previous_hash,
+                    &block.encoded_model,
+                ],
+            )?;
+        }
+
+        Ok(BlockChain { connection })
     }
 
-    pub fn add_block(&mut self, proof: String, encoded_model: String) {
-        let index = self.blocks.len();
-        let timestamp = CString::new(Utc::now().to_rfc3339()).unwrap().into_raw();
-        let proof_cstring = CString::new(proof).unwrap().into_raw();
-        let previous_hash = self.blocks.last().unwrap().proof;
-        let encoded_model_cstring = CString::new(encoded_model).unwrap().into_raw();
+    pub fn save_block(&self, block: &Block) -> Result<(), rusqlite::Error> {
+        self.connection.execute(
+            "INSERT INTO blocks (index, timestamp, proof, hash, previous_hash, encoded_model)
+            VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+            params![
+                block.index,
+                &block.timestamp,
+                block.proof,
+                &block.hash,
+                &block.previous_hash,
+                &block.encoded_model
+            ],
+        )?;
 
-        let hash = CString::new(Block::calculate_hash(index, timestamp, proof_cstring, previous_hash, encoded_model_cstring)).unwrap().into_raw();
+        Ok(())
+    }
 
-        let block = Block::new(index, timestamp, proof_cstring, hash, previous_hash, encoded_model_cstring);
+    pub fn get_block_by_index(&self, index: usize) -> Result<Option<Block>, rusqlite::Error> {
+        let mut smtm = self.connection.prepare(
+            "SELECT index, timestamp, proof, hash, previous_hash, encoded_model
+            FROM blocks WHERE index = ?1",
+        )?;
 
-        self.blocks.push(block);
+        let result = smtm.query_row(params![index], |row| {
+            Ok(Block {
+                index: row.get(0)?,
+                timestamp: row.get(1)?,
+                proof: row.get(2)?,
+                hash: row.get(3)?,
+                previous_hash: row.get(4)?,
+                encoded_model: row.get(5)?,
+            }
+                )
+        });
+
+        match result {
+            Ok(block) => Ok(Some(block)),
+            Err(rusqlite::Error::QueryReturnedNoRows) => Err(rusqlite::Error::QueryReturnedNoRows),
+            Err(err) => Err(err)
+        }
     }
 }
