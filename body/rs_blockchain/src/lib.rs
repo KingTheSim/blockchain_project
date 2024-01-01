c58942971d6a2e698486c7ba21ad6b1c40651db6
simeon <kingthesim@abv.bg>
2024-01-02 00:28:29 +0200

    pow

--- body/rs_blockchain/src/lib.rs
+++ body/rs_blockchain/src/lib.rs
@@ -1,12 +1,16 @@
 use core::time;
+use std::ffi::CStr;
+use std::time::Instant;
 use chrono::{Utc, DateTime};
 use blake3::Hasher;
-use rusqlite::{params, Connection};
+use rusqlite::{params, Connection, ffi::Error};
 use serde::{Serialize, Deserialize};
 use serde_json::json;
 
+const DIFFICULTY: usize = 4;
+
 #[derive(Serialize, Deserialize, Debug)]
-struct Block {
+pub struct Block {
     index: usize,
     timestamp: String,
     proof: usize,
@@ -16,7 +20,8 @@ struct Block {
 }
 
 impl Block {
-    fn new(index: usize, timestamp: String, proof: usize, previous_hash: String, encoded_model: String) -> Self {
+    pub fn new(index: usize, timestamp: String, previous_hash: String, encoded_model: String) -> Self {
+        let proof = Block::proof_of_work(index, previous_hash.clone());
         let hash = Block::calculate_hash(index, &timestamp, proof, &previous_hash, &encoded_model);
 
         Block {
@@ -29,7 +34,7 @@ impl Block {
         }
     }
 
-    fn calculate_hash(index: usize, timestamp: &String, proof: usize, previous_hash: &String, encoded_model: &String) -> String {
+    fn calculate_hash(index: usize, timestamp: &str, proof: usize, previous_hash: &str, encoded_model: &str) -> String {
         let content = format!(
             "{}{}{}{}{}",
             index,
@@ -43,6 +48,32 @@ impl Block {
         hasher.update(content.as_bytes());
         let hash = hasher.finalize();
         hash.to_hex().to_string()
+    }
+
+    fn proof_of_work(index: usize, previous_hash: String) -> usize {
+        let start_time = Instant::now();
+        let mut proof = 0;
+
+        while !Block::is_valid_proof(proof, index, &previous_hash) {
+            proof += 1;
+        }
+
+        let elapsed_time = start_time.elapsed();
+        println!("Proof-of-work completed in {} seconds.", elapsed_time.as_secs_f64());
+        proof
+    }
+
+    fn is_valid_proof(proof: usize, index: usize, previous_hash: &String) -> bool {
+        let guess = format!("{}{}", proof, Utc::now().timestamp_millis());
+        let guess_hash = Block::calculate_hash(
+            index,
+            &Utc::now().to_rfc3339(),
+            proof,
+            &previous_hash,
+            &guess
+        );
+
+        guess_hash.starts_with(&"0".repeat(DIFFICULTY))
     }
 
     fn to_json(&self) -> String {
@@ -159,5 +190,56 @@ impl BlockChain {
             Err(rusqlite::Error::QueryReturnedNoRows) => Err(rusqlite::Error::QueryReturnedNoRows),
             Err(err) => Err(err)
         }
+    }
+
+    pub fn get_last_block(&self) -> Result<Option<Block>, rusqlite::Error> {
+        let mut smtm = self.connection.prepare(
+            "SELECT index, timestamp, proof, hash, previous_hash, encoded_model
+            FROM blocks ORDER BY index DESC LIMIT 1",
+        )?;
+
+        let result = smtm.query_row([], |row| {
+            Ok(Block {
+                index: row.get(0)?,
+                timestamp: row.get(1)?,
+                proof: row.get(2)?,
+                hash: row.get(3)?,
+                previous_hash: row.get(4)?,
+                encoded_model: row.get(5)?,
+            }
+                )
+        });
+
+        match result {
+            Ok(block) => Ok(Some(block)),
+            Err(rusqlite::Error::QueryReturnedNoRows) => Err(rusqlite::Error::QueryReturnedNoRows),
+            Err(err) => Err(err)
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn init_blockchain() -> *mut BlockChain {
+    Box::into_raw(Box::new(BlockChain::new().unwrap()))
+}
+
+#[no_mangle]
+pub extern "C" fn add_block(chain: *mut BlockChain, encoded_model: *const i8) -> () {
+    let chain = unsafe { &mut *chain };
+    let encoded_model_string = unsafe { CStr::from_ptr(encoded_model).to_str().unwrap() };
+    if let Ok(Some(last_block)) = chain.get_last_block() {
+        let new_block = Block::new(
+            last_block.index + 1,
+            Utc::now().to_rfc3339(),
+            last_block.hash.clone(),
+            encoded_model_string.to_string(),
+        );
+
+        match chain.save_block(&new_block) {
+            Ok(_) => println!("Block with index {} saved.", new_block.index),
+            Err(err) => eprintln!("{}", err),
+        }
+    } else {
+        eprintln!("Error getting last block.")
     }
 }
